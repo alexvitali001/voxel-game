d8213b10ed910580feb874b1c4b312f8439b29f4
alexvitali001 <alex.vitali.001@gmail.com>
2023-08-29 21:35:36 -0400

    fps counter

--- src/debugtext/mod.rs
+++ src/debugtext/mod.rs
@@ -1,6 +1,9 @@
 use crate::player::ThisPlayer;
 use crate::position::WorldPosition;
-use bevy::prelude::*;
+use bevy::{
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+    prelude::*,
+};
 #[derive(Component)]
 struct DebugText;
 
@@ -30,22 +33,35 @@ fn setup_debug_text(mut commands: Commands) {
 fn update_debug_text(
     mut text_query: Query<&mut Text, With<DebugText>>,
     player_query: Query<&WorldPosition, With<ThisPlayer>>,
+    diagnostics: Res<DiagnosticsStore>,
 ) {
     let mut text = text_query.single_mut();
+
+    // Position Values
     let player_worldpos = player_query.single();
     let pos = player_worldpos.position;
     let pitch = player_worldpos.pitch.to_degrees();
     let yaw = player_worldpos.yaw.to_degrees();
+
+    // Diagnostics
+    let fps = diagnostics
+        .get(FrameTimeDiagnosticsPlugin::FPS)
+        .unwrap()
+        .smoothed()
+        .unwrap_or_default();
     text.sections[0].value = format!(
-        "X={:.5}, Y={:.5}, Z={:.5}\nPitch={:.1}, Yaw={:.1}",
-        pos.x, pos.y, pos.z, pitch, yaw
+        "X={:.5}, Y={:.5}, Z={:.5}
+            \nPitch={:.1}, Yaw={:.1}
+            \nFPS={:.1}",
+        pos.x, pos.y, pos.z, pitch, yaw, fps
     );
 }
 
 pub struct DebugTextPlugin;
 impl Plugin for DebugTextPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_debug_text)
+        app.add_plugins(FrameTimeDiagnosticsPlugin)
+            .add_systems(Startup, setup_debug_text)
             .add_systems(Update, update_debug_text);
     }
 }
